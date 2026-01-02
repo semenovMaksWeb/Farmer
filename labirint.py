cf74345badca6000ecd03db1ac3f523b26eea5e1
semenov <semenov>
2026-01-02 15:36:55 +0400

    лабиринт

--- labirint.py
+++ labirint.py
@@ -1,22 +1,25 @@
-def run(size = get_world_size() * 2**(num_unlocked(Unlocks.Mazes) - 1)):
-	print(str(North))
+def generatorKeysList(x,y):
+	return "x" + str(x) + "y" + str(y)
+
+def run(size = get_world_size() * 2**(num_unlocked(Unlocks.Mazes) - 1), countMax = 5):
 	listDirection = [North,East,South,West]
 	listDirectionPath = [South,West,North,East]
 	path = []
 	list = {}
+	countCur = 0
 	while(True):
 		# я в лабиринте
 		if get_entity_type() == Entities.Hedge:
 			x = get_pos_x()
 			y = get_pos_y()
 			# Создание клетки и определения путей
-			if str(x) + str(y) not in list:
-				list[str(x) + str(y)] = { }
+			if generatorKeysList(x,y) not in list:
+				list[generatorKeysList(x,y)] = { }
 				for itemDirection in listDirection:
 					if(can_move(itemDirection)):
-						list[str(x) + str(y)][str(itemDirection)] = True   
+						list[generatorKeysList(x,y)][str(itemDirection)] = True   
 					else: 
-						list[str(x) + str(y)][str(itemDirection)] = False 
+						list[generatorKeysList(x,y)][str(itemDirection)] = False 
 			# Движения
 			#x, y = measure()
 			index = -1
@@ -24,24 +27,41 @@ def run(size = get_world_size() * 2**(num_unlocked(Unlocks.Mazes) - 1)):
 			for itemDirection in listDirection:
 				index = index + 1
 				# Путь есть и если дрон пришел не оттуда
-				if list[str(x) + str(y)][str(itemDirection)] and listDirectionPath[index] != itemDirection:
-					list[str(x) + str(y)][str(itemDirection)] = False
+				if list[generatorKeysList(x,y)][str(itemDirection)] and listDirectionPath[index] != itemDirection:
+					list[generatorKeysList(x,y)][str(itemDirection)] = False
 					path.append(listDirectionPath[index])
 					move(itemDirection)
 					checkMove = True
 					break
 
+			# Возвращаемся назад
 			if checkMove == False:
-				move(path[len(path) - 1])
-				path.pop()
-				# Надо возвращаться назад нужно продолжать двигаться пока не путей по path
+				while(True):
+					move(path[len(path) - 1])
+					path.pop()
+					xPath = get_pos_x()
+					yPath = get_pos_y()
+
+					checkMovePath = False
+
+					for itemDirection in listDirection:
+						if list[generatorKeysList(xPath, yPath)][str(itemDirection)]:
+							checkMovePath = True
+					if checkMovePath:
+						break
 				continue
-				 
 						
 		# Это сокровище
 		if get_entity_type() == Entities.Treasure:
-			harvest()
-			   
+			list = {}
+			path = []
+			countCur = countCur + 1
+			if countCur == countMax:
+				countCur = 0
+				harvest()
+			else:
+				use_item(Items.Weird_Substance, size)
+
 		# не лабиринт
 		if get_entity_type() != Entities.Hedge:
 			# не трава
